939f906821daebd5554e4f72c8520df5ff834725
meh <meh.ffff@gmail.com>
2010-09-27 03:28:22 +0200

    Useless manga downloader, lol.

---
+++ ruby/mangadl.rb
@@ -0,0 +1,49 @@
+#! /usr/bin/env ruby
+require 'net/http'
+require 'uri'
+
+MANGA = ARGV.shift || exit
+HOST  = 'http://view.thespectrum.net'
+
+volumes = 1 .. Net::HTTP.get(URI.parse("#{HOST}/series/#{MANGA.downcase}-volume-01.html")).match(%r{(\d+)</option></select>})[1].to_i
+
+puts "#{MANGA.capitalize} exists, downloading."
+
+Dir.mkdir("#{MANGA.capitalize}") rescue nil
+Dir.chdir("#{MANGA.capitalize}")
+
+volumes.each {|volume|
+  directory = "#{MANGA.capitalize} #{'%02d' % [volume]}"
+  Dir.mkdir(directory) rescue nil
+  Dir.chdir(directory)
+
+  puts "Downloading #{directory}."
+
+  pages = 1 .. Net::HTTP.get(URI.parse("#{HOST}/series/#{MANGA.downcase}-volume-#{'%02d' % [volume]}.html")).match(/of (\d+)/)[1].to_i
+
+  pages.each {|page|
+    file = "#{'%03d' % [page]}.jpg"
+
+    if File.exists?(file) && File.size(file) > 0
+      puts "Already have #{page}/#{pages.last} (volume #{volume}/#{volumes.last})."
+      next
+    else
+      puts "Downloading page #{page}/#{pages.last} (volume #{volume}/#{volumes.last})."
+    end
+
+    path = Net::HTTP.get(URI.parse("#{HOST}/series/#{MANGA.downcase}-volume-#{'%02d' % [volume]}.html?ch=Volume+#{'%02d' % [volume]}&page=#{page}")).match(/<img src="(.*?\.jpg)"/)[1] rescue nil
+
+    if !path
+      puts "Couldn't download page #{page}."
+      next
+    end
+
+    file = File.new(file, 'w')
+    file.write(Net::HTTP.get(URI.parse("#{HOST}#{URI.encode(path)}")))
+    file.close
+  }
+
+  Dir.chdir('..')
+}
+
+Dir.chdir('..')
