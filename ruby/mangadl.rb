7b978e7130a460a798ac71a0b4e7d28ff3f3c044
meh <meh.ffff@gmail.com>
2010-09-27 03:38:07 +0200

    [ruby/mangadl.rb] Added some options.

--- ruby/mangadl.rb
+++ ruby/mangadl.rb
@@ -1,16 +1,25 @@
 #! /usr/bin/env ruby
 require 'net/http'
 require 'uri'
+require 'optimus'
 
-MANGA = ARGV.shift || exit
-HOST  = 'http://view.thespectrum.net'
+HOST = 'http://view.thespectrum.net'
+
+opt = Optimus.new {|o|
+  o.set(
+    :long  => 'output',
+    :short => 'o'
+  )
+}
+
+MANGA = opt.arguments.shift || exit
 
 volumes = 1 .. Net::HTTP.get(URI.parse("#{HOST}/series/#{MANGA.downcase}-volume-01.html")).match(%r{(\d+)</option></select>})[1].to_i
 
 puts "#{MANGA.capitalize} exists, downloading."
 
-Dir.mkdir("#{MANGA.capitalize}") rescue nil
-Dir.chdir("#{MANGA.capitalize}")
+Dir.mkdir("#{opt.params[:o] || MANGA.capitalize}") rescue nil
+Dir.chdir("#{opt.params[:o] || MANGA.capitalize}")
 
 volumes.each {|volume|
   directory = "#{MANGA.capitalize} #{'%02d' % [volume]}"
@@ -31,16 +40,20 @@ volumes.each {|volume|
       puts "Downloading page #{page}/#{pages.last} (volume #{volume}/#{volumes.last})."
     end
 
-    path = Net::HTTP.get(URI.parse("#{HOST}/series/#{MANGA.downcase}-volume-#{'%02d' % [volume]}.html?ch=Volume+#{'%02d' % [volume]}&page=#{page}")).match(/<img src="(.*?\.jpg)"/)[1] rescue nil
+    begin
+      path = Net::HTTP.get(URI.parse("#{HOST}/series/#{MANGA.downcase}-volume-#{'%02d' % [volume]}.html?ch=Volume+#{'%02d' % [volume]}&page=#{page}")).match(/<img src="(.*?\.jpg)"/)[1] rescue nil
 
-    if !path
-      puts "Couldn't download page #{page}."
-      next
+      if !path
+        puts "Couldn't download page #{page} (#{"#{HOST}/series/#{MANGA.downcase}-volume-#{'%02d' % [volume]}.html?ch=Volume+#{'%02d' % [volume]}&page=#{page}"}."
+        next
+      end
+
+      file = File.new(file, 'w')
+      file.write(Net::HTTP.get(URI.parse("#{HOST}#{URI.encode(path)}")))
+      file.close
+    rescue Errno::ETIMEDOUT
+      retry
     end
-
-    file = File.new(file, 'w')
-    file.write(Net::HTTP.get(URI.parse("#{HOST}#{URI.encode(path)}")))
-    file.close
   }
 
   Dir.chdir('..')
