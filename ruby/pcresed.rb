a2c1566faf1fcaf1075a23125d1db7f434c381b7
meh <meh@paranoici.org>
2011-09-16 19:50:31 +0200

    rtile: add normal block

--- ruby/pcresed.rb
+++ ruby/pcresed.rb
@@ -2,70 +2,82 @@
 require 'open-uri'
 
 options = {
-  :replace => false,
-  :regexps => [],
-  :files   => []
+	:replace => false,
+	:regexps => [],
+	:files   => []
 }
 
 skip = false
 ARGV.each_with_index {|option, index|
-  if skip
-    skip = false
-    next
-  end
+	if skip
+		skip = false
+		next
+	end
 
-  case option
-    when '-i'
-      options[:replace] = true
+	case option
+		when '-i'
+			options[:replace] = true
 
-    when '-e'
-      options[:regexps].push(ARGV[index + 1])
-      skip = true
+		when '-e'
+			options[:regexps].push(ARGV[index + 1])
+			skip = true
 
-    when /^-/
-      fail "#{option}: Unknown option"
+		when /^-/
+			fail "#{option}: Unknown option"
 
-    else
-      options[:files].push(option)
-  end
+		else
+			options[:files].push(option)
+	end
 }
 
 options[:files].map! {|file|
-  [file, open(file) {|f|
-    f.read
-  }]
+	[file, open(file) {|f|
+		f.read
+	}]
 }
 
 if IO.select([STDIN], nil, nil, 0)
-  options[:files].push [STDIN, STDIN.read]
+	options[:files].push [STDIN, STDIN.read]
 elsif options[:files].empty?
-  fail "What should I work on?"
+	fail "What should I work on?"
 end
 
 options[:files].each {|(path, data)|
-  options[:regexps].each {|regex|
-    case regex
-      when /^s(.)(.*?)\1(.*?)\1([gi]?)$/
-        ($4.include?('g') ? data.method(:gsub!) : data.method(:sub!)).call(Regexp.new($2, $4), $3)
+	options[:regexps].each {|regex|
+		case regex
+			when /^s(.)(.*?)\1(.*?)\1([Ggi]?)$/
+				regex, replace, mode = $2, $3, $4
 
-      when /^d(.)(.*?)\1([gi]?)$/
-        ($4.include?('g') ? data.method(:gsub!) : data.method(:sub!)).call(Regexp.new($2, $4), '')
-    end
-  }
+				if mode.include?(?G)
+					while (mode.include?(?g) ? data.method(:gsub!) : data.method(:sub!)).call(Regexp.new(regex, mode), replace); end
+				else
+					(mode.include?(?g) ? data.method(:gsub!) : data.method(:sub!)).call(Regexp.new(regex, mode), replace)
+				end
+
+			when /^d(.)(.*?)\1([Ggi]?)$/
+				regex, mode = $2, $4
+
+				if mode.include?(?G)
+					while (mode.include?(?g) ? data.method(:gsub!) : data.method(:sub!)).call(Regexp.new(regex, mode), ''); end
+				else
+					(mode.include?(?g) ? data.method(:gsub!) : data.method(:sub!)).call(Regexp.new(regex, mode), '')
+				end
+		end
+	}
 }
 
 if options[:replace]
-  options[:files].each {|(path, data)|
-    next if path == STDIN
+	options[:files].each {|(path, data)|
+		next if path == STDIN
 
-    open(path, 'w') {|f|
-      f.write(data)
-    }
-  }
+		open(path, 'w') {|f|
+			f.write(data)
+		}
+	}
 else
-  options[:files].each {|(path, data)|
-    print data
-  }
+	options[:files].each {|(path, data)|
+		print data
+	}
 
-  STDOUT.flush
+	STDOUT.flush
 end
--- ruby/rtile.rb
+++ ruby/rtile.rb
@@ -5,90 +5,94 @@ require 'X11/simple'
 options = {}
 
 OptionParser.new do |o|
-  options[:config] = "#{ENV['HOME']}/.rtilerc"
+	options[:config] = "#{ENV['HOME']}/.rtilerc"
 
-  o.on '-c', '--config CONFIG', 'The configuration file to use' do |path|
-    options[:config] = path if path
-  end
+	o.on '-c', '--config CONFIG', 'The configuration file to use' do |path|
+		options[:config] = path if path
+	end
 
-  o.on '-D', '--display DISPLAY', 'X display to use' do |name|
-    options[:display] = name
-  end
+	o.on '-D', '--display DISPLAY', 'X display to use' do |name|
+		options[:display] = name
+	end
 
-  o.on '-s', '--select', 'Select a window' do
-    options[:select] = true
-  end
+	o.on '-s', '--select', 'Select a window' do
+		options[:select] = true
+	end
 
-  o.on '-f', '--focus', 'Use focused window as target' do
-    options[:focus] = true
-  end
+	o.on '-f', '--focus', 'Use focused window as target' do
+		options[:focus] = true
+	end
 end.parse!
 
 Class.new {
-  def initialize (display, path, options={})
-    @display = display
-    @options = options
+	def initialize (display, path, options={})
+		@display = display
+		@options = options
 
-    instance_eval File.read(path), path, 1
-  end
+		instance_eval File.read(path), path, 1
+	end
 
-  def display (name=nil, &block)
-    return unless name == @display
+	def display (name=nil, &block)
+		return unless name == @display
 
-    @display = X11::Display.open(name)
+		@display = X11::Display.open(name)
 
-    if @options[:select]
-      @select = @display.select_window(true) if @select
-    elsif @options[:focus]
-      @focus = @display.focused
-    end
+		if @options[:select]
+			@select = @display.select_window(true) if @select
+		elsif @options[:focus]
+			@focus = @display.focused
+		end
 
-    instance_exec @display, &block
-  end
+		instance_exec @display, &block
+	end
 
-  def window (data, &block)
-    return if @select or @focus
+	def window (data, &block)
+		return if @select or @focus
 
-    @display.default_screen.windows.each {|window|
-      block.call(window) if matches?(data, window)
-    }
-  end
+		@display.default_screen.windows.each {|window|
+			block.call(window) if matches?(data, window)
+		}
+	end
 
-  def select (&block)
-    return unless @select
+	def select (&block)
+		return unless @select
 
-    block.call(@select)
-  end
+		block.call(@select)
+	end
 
-  def focus (&block)
-    return unless @focus
+	def focus (&block)
+		return unless @focus
 
-    block.call(@focus)
-  end
+		block.call(@focus)
+	end
 
-  def matches? (data, window)
-    data.any? {|name, value|
-      value = [value].flatten
+	def normal (&block)
+		block.call if !@focus && !@select
+	end
 
-      if name == :name
-        compare_to = window.properties[:WM_CLASS].value
-      elsif name == :title
-        compare_to = [window.title]
-      else
-        raise RuntimeError
-      end rescue next
+	def matches? (data, window)
+		data.any? {|name, value|
+			value = [value].flatten
 
-      value.any? {|what|
-        if what.is_a?(Regexp)
-          compare_to.any? {|compare|
-            compare.match(what)
-          }
-        else
-          compare_to.any? {|compare|
-            what.to_s == compare
-          }
-        end
-      }
-    }
-  end
+			if name == :name
+				compare_to = window.properties[:WM_CLASS].value
+			elsif name == :title
+				compare_to = [window.title]
+			else
+				raise RuntimeError
+			end rescue next
+
+			value.any? {|what|
+				if what.is_a?(Regexp)
+					compare_to.any? {|compare|
+						compare.match(what)
+					}
+				else
+					compare_to.any? {|compare|
+						what.to_s == compare
+					}
+				end
+			}
+		}
+	end
 }.new(options[:display], options[:config], select: options[:select], focus: options[:focus])
