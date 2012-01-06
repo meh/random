0733be615d655a25598368863ba610751ef26cc5
meh <meh@paranoici.org>
2012-01-06 14:34:33 +0100

    ruby/qrencode: forgot to run #parse!

--- ruby/qrencode.rb
+++ ruby/qrencode.rb
@@ -4,79 +4,72 @@
 #           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 #                   Version 2, December 2004
 #
-# Copyleft meh. [http://meh.doesntexist.org | meh@paranoici.org]
-#
 #           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 #  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 #
 # 0. You just DO WHAT THE FUCK YOU WANT TO.
 ####################################################################
 
-
-require 'optimus'
+require 'optparse'
 require 'rqrcode'
 require 'chunky_png'
 
-BORDER = 1
+options = {}
 
-opt = Optimus.new {|o|
-  o.set(
-    :type => :numeric,
+OptionParser.new do |o|
+	options[:border] = 1
+	options[:error]  = ?L
+	options[:size]   = 3
 
-    :long    => 'level',
-    :short   => 'l',
-    :default => 4
-  )
+	o.on '-l', '--level VALUE', Integer, 'the compression level' do |value|
+		options[:level] = value
+	end
 
-  o.set(
-    :long    => 'error',
-    :short   => 'e',
-    :default => 'L'
-  )
+	o.on '-e', '--error VALUE', 'the error level' do |value|
+		options[:error] = value
+	end
 
-  o.set(
-    :long  => 'output',
-    :short => 'o'
-  )
+	o.on '-o', '--output PATH', 'the path to save a PNG' do |path|
+		options[:output] = path
+	end
 
-  o.set(
-    :type => :numeric,
+	o.on '-p', '--pixel SIZE', Integer, 'the pixel size' do |size|
+		options[:pixel] = size
+	end
 
-    :long    => 'pixel',
-    :short   => 'p',
-    :default => 3,
-  )
-}
+	o.on '-b', '--border BORDER', Integer, 'the border size' do |size|
+		options[:border] = size
+	end
+end.parse!
 
 begin
-  code = RQRCode::QRCode.new(opt.arguments.first(), {
-    :size  => opt.params[:level],
-    :level => opt.params[:error].downcase.to_sym
-  })
+  code = RQRCode::QRCode.new(ARGV.first,
+    size:  options[:level],
+    level: options[:error].downcase.to_sym
+  )
 rescue RQRCode::QRCodeRunTimeError => e
   puts e.to_s.capitalize
   exit 1
 end
 
-
-if !opt.params[:output]
+if !options[:output]
   puts "\n\n"
 
-  code.to_s(:true => '██', :false => '  ').lines.each {|line|
+  code.to_s(true: '██', false: '  ').lines.each {|line|
     print "    #{line}"
   }
 
   puts "\n\n\n"
 else
-  size = (BORDER * 2) + (code.module_count)
+  size = (options[:border] * 2) + (code.module_count)
 
   img = ChunkyPNG::Image.new(size, size, ChunkyPNG::Color::WHITE)
 
   code.modules.each_with_index {|row, y|
     row.each_with_index {|col, x|
-      img[x + BORDER, y + BORDER] = ChunkyPNG::Color::BLACK if col
+      img[x + options[:border], y + options[:border]] = ChunkyPNG::Color::BLACK if col
     }
   }
 
-  img.save(opt.params[:output])
+  img.save(options[:output])
 end
