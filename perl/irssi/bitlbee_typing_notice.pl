00c2b0c3dd384ad250a545b45421669b4d8a1472
meh <meh@paranoici.org>
2012-04-30 14:34:42 +0200

    Improve typing support with timeouts per protocol

--- perl/irssi/bitlbee_typing_notice.pl
+++ perl/irssi/bitlbee_typing_notice.pl
@@ -27,7 +27,14 @@ use constant NOT_TYPING => 0;
 use constant TYPING     => 1;
 use constant THINKING   => 2;
 
+use constant TIMEOUT => {
+	msn => 7
+};
+
 my %typing;
+my %timeouts;
+my %protocols;
+my %querying;
 
 sub redraw {
 	my ($from)  = @_;
@@ -43,13 +50,13 @@ sub typing {
 	my ($from, $type, $no_redraw) = @_;
 
 	if ($type == 0) {
-		delete $typing{$from};
+		delete $typing->{$from};
 	}
 	elsif ($type == 1) {
-		$typing{$from} = 1;
+		$typing->{$from} = 1;
 	}
 	elsif ($type == 2) {
-		$typing{$from} = 2;
+		$typing->{$from} = 2;
 	}
 
 	redraw($from) unless $no_redraw;
@@ -60,10 +67,53 @@ Irssi::signal_add 'ctcp msg' => sub {
 
 	return unless $server->isupport('NETWORK') eq 'BitlBee';
 
-	if (my($type) = $msg =~ /TYPING ([0-9])/) {
+	if (my ($type) = $msg =~ /TYPING ([0-9])/) {
+		unless ($protocols->{$address}) {
+			$querying->{$from} = 1;
+
+			$server->command("whois $from");
+		}
+
 		Irssi::signal_stop();
 
 		typing($from, $type);
+
+		if ($timeouts->{$from}) {
+			Irssi::timeout_remove($timeouts->{$from});
+
+			delete $timeouts->{$from};
+		}
+
+		if ($protocols->{$address} && TIMEOUT->{$protocols->{$address}}) {
+			$timeouts->{$from} = Irssi::timeout_add_once(TIMEOUT->{$protocols->{$address}} * 1000, 'typing', $from);
+		}
+	}
+};
+
+Irssi::signal_add_first 'server event' => sub {
+	my ($server, $data) = @_;
+
+	return unless $server->isupport('NETWORK') eq 'BitlBee';
+
+	my ($number, $nick) = $data =~ m/^(\d+) .*? (.*?) /;
+
+	if ($querying->{$nick}) {
+		Irssi::signal_stop() if $number == 301 || $number == 311 || $number == 312;
+
+		if ($number == 312) {
+			if ($data =~ m/:(.*?) /) {
+				my $protocol = $1;
+				my $address  = $server->query_find($nick)->{address};
+
+				$protocols->{$address} = $protocol;
+
+				if (TIMEOUT->{$protocol}) {
+					$timeouts->{$nick} = Irssi::timeout_add_once(TIMEOUT->{$protocol} * 1000, 'typing', $nick);
+				}
+			}
+
+			delete $querying->{$nick};
+		}
 	}
 };
 
@@ -118,8 +168,8 @@ sub typing_notice {
 	my $window                 = Irssi::active_win();
 	my $channel                = $window->get_active_name();
 
-	if (exists($typing{$channel})) {
-		my $mode = $typing{$channel} == 2 ? 'thinking' : 'typing';
+	if (exists $typing->{$channel}) {
+		my $mode = $typing->{$channel} == 2 ? 'thinking' : 'typing';
 
 		$item->default_handler($get_size_only, "{sb $mode}", 0, 1);
 	}
