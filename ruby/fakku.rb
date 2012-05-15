7a65c5a37204455102b3a99e55d736cecb09bfb8
meh <meh@paranoici.org>
2012-05-16 00:07:50 +0200

    Add script to scrap fakku.net

---
+++ ruby/fakku.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/env ruby
+require 'net/http'
+
+link = if matches = ARGV.first.match(%r{fakku.*/manga/(.*?)/?})
+	"http://www.fakku.net/manga/#{matches[1]}/read"
+else
+	"http://www.fakku.net/manga/#{ARGV.first}/read"
+end
+
+cdn = Net::HTTP.get(URI.parse(link))[%r{return '(http://cdn\.fakku\.net.*?)'}, 1]
+
+1.upto(999) {|i|
+	file = "%03d.jpg" % i
+
+	puts "Downloading #{file}"
+
+	File.open(file, ?w) {|f|
+		response = Net::HTTP.get_response(URI.parse("#{cdn}#{file}"))
+
+		if response.is_a?(Net::HTTPSuccess)
+			f.write(response.body)
+		else
+			exit
+		end
+	}
+}
